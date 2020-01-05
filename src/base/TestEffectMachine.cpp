587ba2d5a8f44b700ededf2d1b4a87193ac7f231
juzhen <changchunjunhao@gmail.com>
2020-01-05 20:11:30 +0800

    Apply UT function to base/TestEffectMachine

--- src/base/TestEffectMachine.cpp
+++ src/base/TestEffectMachine.cpp
@@ -3,26 +3,30 @@
 #include <stack>
 #include "stdio.h"
 
+#include "../unit_test/UT.h"
+
 #include "Effect.h"
 #include "EffectMachine.h"
 #include "GameObject.h"
 
-bool testAddHealth();
-bool testAddAttach();
-bool testBoth();
-bool testDisable();
+using namespace UT;
+
+Report testAddHealth();
+Report testAddAttach();
+Report testBoth();
+Report testDisable();
 bool concatCommand(std::string& command, int code);
-bool assertTrue(bool detection, std::string errorMessage);
 
 int main()
 {
-    bool pass = true;
-    pass = testAddHealth() && pass;
-    pass = testAddAttach() && pass;
-    pass = testBoth() && pass;
-    pass = testDisable() && pass;
-    printf("pass: %s\n", pass ? "pass" : "fail");
-    return pass ? 0 : 1;
+    Report report;
+    report.mergeReport(testAddHealth());
+    report.mergeReport(testAddAttach());
+    report.mergeReport(testBoth());
+    report.mergeReport(testDisable());
+    Result result = report.getResult();
+    std::cout << result.toString();
+    return result.totalPass() ? 0 : 1;
 }
 
 void addHealth(std::string& command, int increament)
@@ -41,21 +45,23 @@ void addAttack(std::string& command, int increment)
     concatCommand(command, EffectMachine::Code::NONE);
 }
 
-bool testAddHealth()
+Report testAddHealth()
 {
-    std::string command;
-    addHealth(command, 1);
+    Report report;
+    std::string command; addHealth(command, 1);
     Effect effect(std::string("Add Health"), command, std::string("Add 1 Health"));
     EffectMachine machine(effect);
     GameObject me(2, 3);
     std::stack<GameObject*> s;
     s.push(&me);
     machine.apply(s);
-    return assertTrue(me.getHealth() == 3, "Error: health is not 3\n");
+    report.addTest(Test::assertEquals(me.getHealth(), 3));
+    return report;
 }
 
-bool testAddAttach()
+Report testAddAttach()
 {
+    Report report;
     std::string command;
     addAttack(command, 1);
     Effect effect(std::string("Add Attack"), command, std::string("Add 1 Attach"));
@@ -64,11 +70,13 @@ bool testAddAttach()
     std::stack<GameObject*> s;
     s.push(&me);
     machine.apply(s);
-    return assertTrue(me.getAttack() == 4, "Error: attack is not 4\n");
+    report.addTest(Test::assertEquals(me.getAttack(), 4));
+    return report;
 }
 
-bool testBoth()
+Report testBoth()
 {
+    Report report;
     std::string command;
     addAttack(command, 1);
     concatCommand(command, EffectMachine::Code::PUSH);
@@ -79,12 +87,14 @@ bool testBoth()
     std::stack<GameObject*> s;
     s.push(&me);
     machine.apply(s);
-    return assertTrue(me.getAttack() == 4, "Error: attack is not 4\n") &&
-            assertTrue(me.getHealth() == 3, "Error: health is not 3\n");
+    report.addTest(Test::assertEquals(me.getAttack(), 4));
+    report.addTest(Test::assertEquals(me.getHealth(), 3));
+    return report;
 }
 
-bool testDisable()
+Report testDisable()
 {
+    Report report;
     std::string addHealthDesc("Add 1 Health");
     std::string addHealthCommand; addHealth(addHealthCommand, 1);
     Effect addHealthEffect(addHealthDesc, addHealthCommand, addHealthDesc);
@@ -96,39 +106,26 @@ bool testDisable()
     GameObject me(2, 3);
     GameObject::applyEffect(&me, Effect::TYPE::ONCE, addHealthEffect);
     GameObject::applyEffect(&me, Effect::TYPE::BUFF, addAttackEffect);
-    bool result = assertTrue(me.getAttack() == 4, "Error: attack is not 4\n");
-    result = assertTrue(me.getHealth() == 3, "Error: health is not 3\n") && result;
+    report.addTest(Test::assertEquals(me.getAttack(), 4));
+    report.addTest(Test::assertEquals(me.getHealth(), 3));
 
     std::string expectDesc;
     expectDesc.append(addHealthDesc); expectDesc.append("\n");
     expectDesc.append(addAttackDesc); expectDesc.append("\n");
     std::string currentDesc = me.showEffectList();
-    std::stringstream error_message;
-    error_message << "desc not equal: \"" << expectDesc << "\"\n ===== \n\"" << currentDesc << "\"";
-    result = assertTrue(expectDesc.compare(currentDesc) == 0, error_message.str()) && result;
+    report.addTest(Test::assertEquals(expectDesc, currentDesc));
 
     me.setDisable();
-    result = assertTrue(me.getAttack() == 4, "Error: after disabled, attack should be 4\n") && result;
-    result = assertTrue(me.getHealth() == 2, "Error: after disabled, attach should be 2") && result;
+    report.addTest(Test::assertEquals(me.getAttack(), 4));
+    report.addTest(Test::assertEquals(me.getHealth(), 2));
     std::string expectDesc2; expectDesc2.append(addAttackDesc); expectDesc2.append("\n");
     std::string currentDesc2 = me.showEffectList();
-    std::stringstream error_message2;
-    error_message2 << "after disable, desc not equal: \"" << expectDesc2 << "\"\n ===== \n\"" << currentDesc2 << "\"";
-    result = assertTrue(expectDesc2.compare(currentDesc2) == 0, error_message2.str()) && result;
+    report.addTest(Test::assertEquals(expectDesc2, currentDesc2));
 
-    return result;
+    return report;
 }
 
 bool concatCommand(std::string& command, int code)
 {
     command.append(1, code);
-}
-
-bool assertTrue(bool detection, std::string errorMessage)
-{
-    if (!detection)
-    {
-        std::cout << errorMessage << std::endl;
-    }
-    return detection;
 }
