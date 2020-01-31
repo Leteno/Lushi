7709ef6cce1f600b0b29952a3ca120cc327ca0be
juzhen <changchunjunhao@gmail.com>
2020-01-31 22:52:21 +0800

    Add UT for buildInstructionCode.py

--- tools/gen-code/buildInstructionCode.py
+++ tools/gen-code/buildInstructionCode.py
@@ -56,16 +56,28 @@ foreach_obj obj {
     sys.exit(-1)
 
 def main():
-    content = getContent()
+    test()
+
+def test():
+    content = getTestContent()
     pResult = parser.parse(content)
     gResult = codeGen.gen(pResult)
-    printCodes(gResult)
+    compare(gResult, expectTestResult())
+    print("pass!")
+
+def compare(codes, expectCodes):
+    lCodes, lExpect = len(codes), len(expectCodes)
+    detectSize = min(lCodes, lExpect)
+    for i in range(detectSize):
+        a, b = codes[i], expectCodes[i]
+        assert a == b, "code dismatch in %d, %s vs %s" % (i, a, b)
+    assert lCodes == lExpect, "code len dismatch: %d %d" % (lCodes, lExpect)
 
 def printCodes(codes):
     for code in codes:
         print(code)
 
-def getContent():
+def getTestContent():
     return """
 sum = 0;
 count = 0;
@@ -86,6 +98,9 @@ foreach_obj obj {
     }
 }
 """
+
+def expectTestResult():
+    return ['PUSH 0', 'PUSH 0', 'PUSH 0', 'PUSH 0', 'PUSH 0', 'STORE 0', 'POP', 'PUSH 0', 'STORE 1', 'POP', 'RESET_OBJ', 'MEET_END_OBJ', 'JMP_IF_FALSE 2', 'JMP 14', 'LOAD_OBJ', 'LOAD 0', 'GET_HEALTH', 'ADD', 'STORE 0', 'POP', 'LOAD 1', 'PUSH 1', 'ADD', 'STORE 1', 'POP', 'MOVE_ON_OBJ', 'JMP -15', 'LOAD 0', 'LOAD 1', 'DIV', 'STORE 2', 'POP', 'RESET_OBJ', 'MEET_END_OBJ', 'JMP_IF_FALSE 2', 'JMP 23', 'LOAD_OBJ', 'GET_HEALTH', 'STORE 3', 'POP', 'LOAD 3', 'LOAD 2', 'LT', 'JMP_IF_FALSE 5', 'LOAD 3', 'PUSH 10', 'ADD', 'SET_HEALTH', 'LOAD 3', 'LOAD 2', 'GT', 'JMP_IF_FALSE 5', 'LOAD 3', 'PUSH 10', 'MINUS', 'SET_HEALTH', 'MOVE_ON_OBJ', 'JMP -24']
 
 if __name__ == "__main__":
     main()
