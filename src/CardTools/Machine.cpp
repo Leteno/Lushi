97deefbbe42c0e7d53e85711caf7e4e04690aa54
zhengjunhao <changchunjunhao@gmail.com>
2020-03-08 01:32:47 +0800

    CardTools: resizing UI

--- src/CardTools/Machine.cpp
+++ src/CardTools/Machine.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Card Tool");
-    gtk_window_set_default_size(GTK_WINDOW(window), 550, 400);
+    gtk_widget_set_size_request(window, 900, 400);
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
