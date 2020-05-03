270d4fd1a18e960112a6879f28f3b2455a34133e
zhengjunhao <changchunjunhao@gmail.com>
2020-05-03 11:53:49 +0800

    Add Test on Sequence

--- src/machine/TestSequence.cpp
+++ src/machine/TestSequence.cpp
@@ -5,7 +5,7 @@
 #include "Sequence.h"
 #include "../unit_test/UT.h"
 
-using namespace machine;
+using namespace machine::Sequence;
 
 UT::Report testSequenceReadAndWrite();
 
@@ -20,57 +20,81 @@ UT::Report testSequenceReadAndWrite()
 {
     UT::Report report;
 
-/*    
+    struct TestData {
+        Code code;
+        Value value;
+    };
+    TestData tests[] = {
+        {
+            Code::PUSH,
+            {
+                Value::INT, 1
+            }
+        },
+        {
+            Code::SAY,
+            {
+                Value::STRING, 0, "hello world"
+            }
+        },
+        {
+            Code::GT,
+            {
+                Value::NONE, 0
+            }
+        },
+        {
+            Code::JMP_IF_FALSE,
+            {
+                Value::INT, 123
+            }
+        }
+    };
 
-    // PUSH 1
-    pBuffer = Sequence::writeCode(pBuffer, Sequence::Code::PUSH);
-    Sequence::Value one;
-    one.type = Sequence::Value::INT;
-    one.intVal = 1;
-    pBuffer = Sequence::writeValue(pBuffer, one);
+    for (int i = 0, size = sizeof(tests) / sizeof(TestData); i < size; ++i)
+    {
+        Code code = tests[i].code;
+        Value value = tests[i].value;
 
-    // SAY "hello world"
-    pBuffer = Sequence::writeCode(pBuffer, Sequence::Code::SAY);
-    Sequence::Value hello;
-    hello.type = Sequence::Value::STRING;
-    hello.stringVal = "hello world";
-    pBuffer = Sequence::writeValue(pBuffer, hello);
+        Instruction instruction;
+        instruction.code = code;
+        instruction.value = value;
 
-    // GT NON
-    pBuffer = Sequence::writeCode(pBuffer, Sequence::Code::GT);
-    Sequence::Value gtNone;
-    gtNone.type = Sequence::Value::NONE;
-    pBuffer = Sequence::writeValue(pBuffer, gtNone);
+        Parcel parcel(1024);
+        instruction.writeToParcel(&parcel);
+        Instruction newInst;
+        newInst.readFromParcel(&parcel);
+        report.addTest(UT::Test::assertTrue(
+            parcel.meetEnd()
+        ));
 
-    // JMP_IF_FALSE
-    pBuffer = Sequence::writeCode(pBuffer, Sequence::Code::JMP_IF_FALSE);
-    Sequence::Value jumpDest;
-    jumpDest.type = Sequence::Value::INT;
-    jumpDest.intVal = 12306;
-    pBuffer = Sequence::writeValue(pBuffer, jumpDest);
+        report.addTest(UT::Test::assertEquals(
+            newInst.code, code
+        ));
+        report.addTest(UT::Test::assertEquals(
+            newInst.value.type, value.type
+        ));
+        switch (value.type)
+        {
+        case Value::INT:
+            report.addTest(UT::Test::assertEquals(
+                newInst.value.intVal, value.intVal
+            ));
+            break;
+        case Value::STRING:
+            report.addTest(UT::Test::assertEquals(
+                newInst.value.stringVal, value.stringVal
+            ));
+            break;
+        case Value::NONE:
+            break;
+        default:
+            // Fail Delibery
+            report.addTest(UT::Test::assertEquals(
+                value.type, Value::INT
+            ));
+        }
+    }
 
-    *pBuffer = '\0'; // add end
-
-    Sequence seq(type, buffer);
-    std::list<Sequence::Instruction> instructionList = seq.getSequence();
-    assert(instructionList.size() == 4);
-    auto it = instructionList.begin();
-
-    Sequence::Instruction add1 = *it; it++;
-    report.addTest(UT::Test::assertEquals(add1.code, Sequence::Code::PUSH));
-    report.addTest(UT::Test::assertEquals(add1.value.intVal, 1));
-
-    Sequence::Instruction sayHello = *it; it++;
-    report.addTest(UT::Test::assertEquals(sayHello.code, Sequence::Code::SAY));
-    report.addTest(UT::Test::assertEquals(sayHello.value.stringVal, "hello world"));
-
-    Sequence::Instruction gtNoneTest = *it; it++;
-    report.addTest(UT::Test::assertEquals(gtNoneTest.code, Sequence::Code::GT));
-    report.addTest(UT::Test::assertEquals(gtNoneTest.value.type, Sequence::Value::Type::NONE));
-
-    Sequence::Instruction jmp = *it; it++;
-    report.addTest(UT::Test::assertEquals(jmp.code, Sequence::Code::JMP_IF_FALSE));
-    report.addTest(UT::Test::assertEquals(jmp.value.intVal, 12306));
-*/
     return report;
 }
