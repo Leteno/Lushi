0878525762a42aedec5cf4f3dd2b9b25202fb8bc
zhengjunhao <changchunjunhao@gmail.com>
2020-05-03 11:59:10 +0800

    CardEffect: replace with Sequence.readFromParcel/writeToParcel

--- src/card/CardEffect.cpp
+++ src/card/CardEffect.cpp
@@ -20,25 +20,7 @@ void CardEffect::readFromParcel(Parcel* parcel)
     for (int i = 0; i < size; ++i)
     {
         Sequence::Instruction instruction;
-        int type = parcel->readInt();
-        switch (type)
-        {
-        case Sequence::Value::INT:
-            instruction.value.type = Sequence::Value::INT;
-            instruction.value.intVal = parcel->readInt();
-            break;
-        case Sequence::Value::STRING:
-            instruction.value.type = Sequence::Value::STRING;
-            instruction.value.stringVal = parcel->readString();
-            break;
-        case Sequence::Value::NONE:
-            instruction.value.type = Sequence::Value::NONE;
-            break;
-        default:
-            // Fail definitely
-            assert(type == Sequence::Value::INT);
-        }
-        instruction.code = Sequence::Code(parcel->readInt());
+        instruction.readFromParcel(parcel);
         instructionList.push_back(instruction);
     }
 }
@@ -51,21 +33,6 @@ void CardEffect::writeToParcel(Parcel* parcel)
         it != instructionList.end();
         ++it)
     {
-        parcel->writeInt(it->value.type);
-        switch (it->value.type)
-        {
-        case Sequence::Value::INT:
-            parcel->writeInt(it->value.intVal);
-            break;
-        case Sequence::Value::STRING:
-            parcel->writeString(it->value.stringVal);
-            break;
-        case Sequence::Value::NONE:
-            break;
-        default:
-            // Fail definitely
-            assert(it->value.type == Sequence::Value::INT);
-        }
-        parcel->writeInt(it->code);
+        it->writeToParcel(parcel);
     }
 }
