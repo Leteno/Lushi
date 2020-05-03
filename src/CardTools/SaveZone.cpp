d0df953ca3e77533eea0a9265f026dbd727cbcff
zhengjunhao <changchunjunhao@gmail.com>
2020-05-03 16:33:54 +0800

    UI of SaveZone

--- src/CardTools/SaveZone.cpp
+++ src/CardTools/SaveZone.cpp
@@ -2,16 +2,42 @@
 #include "SaveZone.h"
 
 using namespace CardTools;
+using namespace card;
+
+static void onSaveButtonClicked(GtkWidget* view, SaveZone* saveZone);
 
 SaveZone::SaveZone(GtkWidget* window)
     : mWindow(window)
 {
-    mRoot = gtk_table_new(1, 1, TRUE);
-    GtkWidget* textView = gtk_label_new_with_mnemonic("Save Zone");
-    gtk_container_add(GTK_CONTAINER(mRoot), textView);
+    mRoot = gtk_table_new(12, 12, TRUE);
+
+    GtkWidget* title = gtk_label_new_with_mnemonic("Save Zone");
+    gtk_table_attach_defaults(GTK_TABLE(mRoot), title, 4, 8, 0, 1);
+
+    GtkWidget* nameLabel = gtk_label_new_with_mnemonic("Name:");
+    gtk_table_attach_defaults(GTK_TABLE(mRoot), nameLabel, 2, 5, 2, 3);
+    GtkWidget* nameText = gtk_entry_new();
+    gtk_table_attach_defaults(GTK_TABLE(mRoot), nameText, 7, 10, 2, 3);
+
+    GtkWidget* saveButton = gtk_button_new_with_label("Save");
+    g_signal_connect(saveButton, "clicked", G_CALLBACK(onSaveButtonClicked), this);
+    gtk_table_attach_defaults(GTK_TABLE(mRoot), saveButton, 7, 10, 5, 6);
 }
 
 GtkWidget* SaveZone::getRoot()
 {
     return mRoot;
+}
+
+void SaveZone::save()
+{
+    printf("onSaveButton clicked\n");
+}
+
+void SaveZone::saveToFile(CardEffect cardEffect)
+{}
+
+static void onSaveButtonClicked(GtkWidget* view, SaveZone* saveZone)
+{
+    saveZone->save();
 }
--- src/CardTools/SaveZone.h
+++ src/CardTools/SaveZone.h
@@ -2,12 +2,19 @@
 
 #include <gtk/gtk.h>
 
+#include "../card/CardEffect.h"
+
+using namespace card;
+
 namespace CardTools {
     class SaveZone {
         public:
             SaveZone(GtkWidget* window);
             GtkWidget* getRoot();
+            void save();
         private:
+            void saveToFile(CardEffect cardEffect);
+
             GtkWidget* mWindow;
             GtkWidget* mRoot;
     };
